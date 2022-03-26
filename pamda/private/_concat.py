37018205850805c9f1f0ce001a66d7cae11e25ca
zyd <zydmayday@gmail.com>
2022-03-26 22:04:00 +0900

    add append method

---
+++ pamda/private/_concat.py
@@ -0,0 +1,2 @@
+def _concat(set1, set2):
+  return set1 + set2
---
+++ test/test_append.py
@@ -0,0 +1,20 @@
+import unittest
+
+import pamda as R
+
+"""
+https://github.com/ramda/ramda/blob/master/test/append.js
+"""
+
+
+class TestAppend(unittest.TestCase):
+  def test_adds_the_element_to_the_end_of_the_list(self):
+    self.assertEqual(['x', 'y', 'z'], R.append('z', ['x', 'y']))
+    self.assertEqual(['x', 'y', ['a', 'z']], R.append(['a', 'z'], ['x', 'y']))
+
+  def test_works_on_empty_list(self):
+    self.assertEqual([1], R.append(1, []))
+
+
+if __name__ == '__main__':
+  unittest.main()
