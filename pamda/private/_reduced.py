2e987e47d557026397a0669ebae407733ebeb505
zyd <zydmayday@gmail.com>
2022-04-03 22:36:15 +0900

    add reduced method

--- pamda/private/_reduced.py
+++ pamda/private/_reduced.py
@@ -2,12 +2,10 @@ from ._helper import getAttribute
 
 
 def _reduced(x):
-  if isinstance(x, dict) and getAttribute(x, '@@transducer/reduced'):
+  if getAttribute(x, '@@transducer/reduced'):
     return x
   else:
     return {
-        {
-            '@@transducer/value': x,
-            '@@transducer/reduced': True
-        }
+        '@@transducer/value': x,
+        '@@transducer/reduced': True
     }
---
+++ pamda/reduced.py
@@ -0,0 +1,4 @@
+from .private._curry1 import _curry1
+from .private._reduced import _reduced
+
+reduced = _curry1(_reduced)
