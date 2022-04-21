cde5c37f53c55d900a3593287e187555eb012732
zyd <zydmayday@gmail.com>
2022-04-21 10:32:57 +0900

    add method clone

---
+++ ramda/clone.py
@@ -0,0 +1,14 @@
+from .private._clone import _clone
+from .private._curry1 import _curry1
+from .private._helper import getAttribute
+from .private._isFunction import _isFunction
+
+
+def inner_clone(value):
+  if _isFunction(getAttribute(value, 'clone')):
+    return value.clone()
+  else:
+    return _clone(value, True)
+
+
+clone = _curry1(inner_clone)
