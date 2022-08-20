3d9309f7a6903ef056cb47e7262a223674918a7d
zyd <zydmayday@gmail.com>
2022-08-20 17:21:15 +0900

    add all lens test cases

--- test/test_lens.py
+++ test/test_lens.py
@@ -14,8 +14,8 @@ alice = {
 }
 
 nameLens = R.lens(R.prop('name'), R.assoc('name'))
-# addressLens = R.lensProp('address')
-# headLens = R.lensIndex(0)
+addressLens = R.lensProp('address')
+headLens = R.lensIndex(0)
 dogLens = R.lensPath(['pets', 'dog'])
 
 
@@ -31,6 +31,39 @@ class TestLens(unittest.TestCase):
         'address': ['22 Walnut St', 'San Francisco', 'CA'],
         'pets': {'dog': 'joker', 'cat': 'batman'}
     }, R.over(nameLens, R.toUpper, alice))
+
+    self.assertEqual({
+        'name': 'Alice Smith',
+        'address': ['22 Walnut St', 'San Francisco', 'CA'],
+        'pets': {'dog': 'joker', 'cat': 'batman'}
+    }, R.set(nameLens, 'Alice Smith', alice))
+
+  def test_may_be_applied_to_a_lens_created_by_lensIndex(self):
+    self.assertEqual('22 Walnut St', R.view(headLens, alice['address']))
+
+    self.assertEqual(['22 WALNUT ST', 'San Francisco', 'CA'], R.over(headLens, R.toUpper, alice['address']))
+
+    self.assertEqual(['52 Crane Ave', 'San Francisco', 'CA'], R.set(headLens, '52 Crane Ave', alice['address']))
+
+  def test_may_be_applied_to_composed_lenses(self):
+    streetLens = R.compose(addressLens, headLens)
+    dogLens = R.compose(R.lensProp('pets'), R.lensProp('dog'))
+
+    self.assertEqual(R.view(dogLens, alice), R.view(R.lensPath(['pets', 'dog']), alice))
+
+    self.assertEqual('22 Walnut St', R.view(streetLens, alice))
+
+    self.assertEqual({
+        'name': 'Alice Jones',
+        'address': ['22 WALNUT ST', 'San Francisco', 'CA'],
+        'pets': {'dog': 'joker', 'cat': 'batman'}
+    }, R.over(streetLens, R.toUpper, alice))
+
+    self.assertEqual({
+        'name': 'Alice Jones',
+        'address': ['52 Crane Ave', 'San Francisco', 'CA'],
+        'pets': {'dog': 'joker', 'cat': 'batman'}
+    }, R.set(streetLens, '52 Crane Ave', alice))
 
 
 if __name__ == '__main__':
